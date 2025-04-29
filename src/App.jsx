708e51093a68a4d532bd10a3af8bba7ebfce1b2e
Sedanur CEYLAN <sdnrcyln2@gmail.com>
2025-04-29 22:29:28 +0300

    ik projesi

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import WorkForm from "./components/WorkForm";
+import Router from "./routes/router";
+
+function App() {
+  return (
+    <>
+      <Router />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
